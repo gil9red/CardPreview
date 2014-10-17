434f2fc1be02c674deb9aedbeaa8ee13eada0028
ipetrash <gil9red@gmail.com>
2014-10-17 16:24:35 +0600

    Signed-off-by: ipetrash <gil9red@gmail.com>

--- designtitem.h
+++ designtitem.h
@@ -7,9 +7,15 @@
 #include "fullcardid1.h"
 
 
+namespace TypeDesignItem {
+    const int TEXT_DESIGN_ITEM = 0;
+    const int IMAGE_DESIGN_ITEM = 1;
+}
+
+
 class DesigntTextItem: public QGraphicsSimpleTextItem
 {
-public :
+public:
     enum CardSide
     {
         Front_Side,
@@ -19,6 +25,10 @@ public :
 
 public:
     DesigntTextItem(QGraphicsItem * parent = 0);
+    virtual int type() const {
+        return TypeDesignItem::TEXT_DESIGN_ITEM;
+    }
+
     void setCard(FullCardID1 * c);
 
     void setTextSize(qreal size);
@@ -44,65 +54,70 @@ protected:
 };
 
 
-//class DesigntImageItem: public QGraphicsPixmapItem
-//{
-//public:
-//    DesigntImageItem(QGraphicsItem * parent = 0)
-//        : QGraphicsPixmapItem(parent),
-//          cardID1(0) {
+class DesigntImageItem: public QGraphicsPixmapItem
+{
+public:
+    DesigntImageItem(QGraphicsItem * parent = 0)
+        : QGraphicsPixmapItem(parent),
+          card(0) {
 
-//        setFlags(QGraphicsItem::ItemIsMovable
-//                 | QGraphicsItem::ItemIsSelectable
-//                 | QGraphicsItem::ItemSendsGeometryChanges);
+        setFlags(QGraphicsItem::ItemIsMovable
+                 | QGraphicsItem::ItemIsSelectable
+                 | QGraphicsItem::ItemSendsGeometryChanges);
 
-//        setTransformationMode(Qt::SmoothTransformation);
-//    }
+        setTransformationMode(Qt::SmoothTransformation);
+    }
 
-//    void setCard(CardID1 * c) {
-//        cardID1 = c;
-//    }
+    virtual int type() const {
+        return TypeDesignItem::IMAGE_DESIGN_ITEM;
+    }
 
-//    void setImage(const QString & path) {
-//        imagePath = path;
-//        originalPixmap.load(imagePath);
-//    }
-//    QString getImagePath() const {
-//        return imagePath;
-//    }
+    void setCard(FullCardID1 * c) {
+        card = c;
+    }
 
-//    void setImageSize(qreal width, qreal height) {
-//        setPixmap(originalPixmap.scaled(width, height,
-//                                        Qt::IgnoreAspectRatio,
-//                                        Qt::SmoothTransformation));
-//    }
-//    QSizeF imageSize() const {
-//        return pixmap().size();
-//    }
+    void setImage(const QString & path) {
+        imagePath = path;
+        originalPixmap.load(imagePath);
+    }
+    QString getImagePath() const {
+        return imagePath;
+    }
 
-//private:
-//    CardID1 * cardID1;
-//    QPixmap originalPixmap;
-//    QString imagePath;
+    void setImageSize(qreal width, qreal height) {
+        setPixmap(originalPixmap.scaled(width, height,
+                                        Qt::IgnoreAspectRatio,
+                                        Qt::SmoothTransformation));
+    }
+    QSizeF imageSize() const {
+        return pixmap().size();
+    }
 
-//protected:
-//    QVariant itemChange(GraphicsItemChange change, const QVariant &value) {
-//        if (change == QGraphicsItem::ItemPositionChange && cardID1) {
-//            // value это новое положение.
-//            QPointF newPos = value.toPointF();
-//            const QRectF rect = cardID1->boundingRect();
-//            const QRectF area(newPos, boundingRect().size());
+private:
+    FullCardID1 * card;
+    QPixmap originalPixmap;
+    QString imagePath;
 
-//            if (!rect.contains(area)) {
-//                // Сохраняем элемент внутри прямоугольника сцены.
-//                const qreal width = boundingRect().size().width();
-//                const qreal height = boundingRect().size().height();
-//                newPos.setX(qMin(rect.right() - width, qMax(newPos.x(), rect.left())));
-//                newPos.setY(qMin(rect.bottom() - height, qMax(newPos.y(), rect.top())));
-//                return newPos;
-//            }
-//        }
-//        return QGraphicsPixmapItem::itemChange(change, value);
-//    }
-//};
+protected:
+    QVariant itemChange(GraphicsItemChange change, const QVariant &value) {
+        if (change == QGraphicsItem::ItemPositionChange) {
+            // value это новое положение.
+            QPointF newPos = value.toPointF();
+            const QRectF rect = card->boundingRect();
+            const QRectF area(newPos, boundingRect().size());
+
+            if (!rect.contains(area)) {
+                // Сохраняем элемент внутри прямоугольника сцены.
+                const qreal width = boundingRect().size().width();
+                const qreal height = boundingRect().size().height();
+
+                newPos.setX(qMin(rect.right() - width, qMax(newPos.x(), rect.left())));
+                newPos.setY(qMin(rect.bottom() - height, qMax(newPos.y(), rect.top())));
+                return newPos;
+            }
+        }
+        return QGraphicsPixmapItem::itemChange(change, value);
+    }
+};
 
 #endif // DESIGNTITEM_H
