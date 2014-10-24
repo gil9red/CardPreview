808fde00249c79ce0cfcd86b0f6e1401c2940fa8
ipetrash <gil9red@gmail.com>
2014-10-24 17:50:45 +0600

    Добавлены функции (java) для перевода из миллиметров в пиксели.
    Signed-off-by: ipetrash <gil9red@gmail.com>

--- cardid1.cpp
+++ cardid1.cpp
@@ -12,6 +12,48 @@ CardID1::CardID1(QGraphicsItem *parent, QGraphicsScene *scene)
 //    int PixelHeightDimesion = myWidget->logicalDpiY(); //width dots per inch
 //    double inch = (double)myWidthInPixel / (double)PixelWidthDimension;
 //
+//double x_fromMMtoPixels(double mm) {
+//    final double dm = mm / 25.4; // Миллиметры в дюймы
+//    final double pixels = dm * QApplication.desktop().logicalDpiX();
+//    return pixels;
+//}
+//double y_fromMMtoPixels(double mm) {
+//    final double dm = mm / 25.4; // Миллиметры в дюймы
+//    final double pixels = dm * QApplication.desktop().logicalDpiY();
+//    return pixels;
+//}
+//QPointF fromMMtoPixels(double x, double y) {
+//    x = x_fromMMtoPixels(x);
+//    y = y_fromMMtoPixels(y);
+//    return new QPointF(x, y);
+//}
+//QPointF fromMMtoPixels(QPointF pos_mm) {
+//    return fromMMtoPixels(pos_mm.x(), pos_mm.y());
+//}
+
+//QRectF fromMMtoPixels(double x, double y, double w, double h) {
+//    x = x_fromMMtoPixels(x);
+//    y = y_fromMMtoPixels(y);
+//    w = x_fromMMtoPixels(w);
+//    h = y_fromMMtoPixels(h);
+//    return new QRectF(x, y, w, h);
+//}
+//QRectF fromMMtoPixels(QRectF rect) {
+//    final double x = rect.x();
+//    final double y = rect.y();
+//    final double w = rect.width();
+//    final double h = rect.height();
+//    return fromMMtoPixels(x, y, w, h);
+//}
+//QSize fromMMtoPixels(QSize size) {
+//    final double w = (double) size.width();
+//    final double h = (double) size.height();
+//    final int x = (int) x_fromMMtoPixels(w);
+//    final int y = (int) y_fromMMtoPixels(h);
+//    return new QSize(x, y);
+//}
+//
+//
 // https://en.wikipedia.org/wiki/ISO/IEC_7810 :
 // "The ID-1 format specifies a size of 85.60 × 53.98 mm (3.370 × 2.125 in) and rounded corners with a radius of 2.88–3.48 mm."
 //
